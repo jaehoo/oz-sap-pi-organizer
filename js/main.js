f38a768bd5e43a0c1c8b54bc5696a60733690e57
jaehoo <jaehoo@gmail.com>
2013-10-06 20:35:08 -0500

    Support Jquery UI

--- js/main.js
+++ js/main.js
@@ -1,9 +1,285 @@
-var OZ = OZ || '';
+var OZ = OZ || {};
+
+OZ.sapOrganizer = OZ.sapOrganizer || {};
+
+OZ.sapOrganizer.getData = function (){
+
+    console.log('Calling... getData');
+
+    var priv={};
+
+    // Get Cols
+
+    $.ajax({
+        url: 'js/cols.json',
+        type: 'GET',
+        dataType: 'json',
+        contentType: "application/json; charset=utf-8",
+        //data: priv.options.urlData,
+        async: false,
+        success: function (data) {
+            priv.cols=data.cols[0];
+            //console.log(priv.data);
+
+        },
+        error: function (err) {
+            console.log('request error: '.f(err));
+        }
+    });
+
+    $.ajax({
+        url: 'js/rows.json',
+        type: 'GET',
+        dataType: 'json',
+        contentType: "application/json; charset=utf-8",
+        //data: priv.options.urlData,
+        async: false,
+        success: function (data) {
+            priv.data=data;
+            console.log(priv.data);
+
+        },
+        error: function (err) {
+            console.log('request error: '.f(err));
+        }
+    });
+
+    return OZ.sapOrganizer.jsonFormat(priv.data);
 
 
 
-//Generates some data. This step is of course normally done by your web server.
-function getData() {
+//    var url = "js/data.json";
+//
+//    var jqxhr =$.getJSON(url, function (json) {
+//        console.log("X:"+json.cols.id.tooltip);
+//        masterData= json;
+//
+//        //OZ.sapOrganizer.buildTable(masterData);
+//    }) .done(function() {
+//
+//            console.log( "second success" );
+//            console.log("num rows:"+masterData.rows.length);
+//            //this.result=OZ.sapOrganizer.jsonFormat(this.result);
+//            //OZ.sapOrganizer.buildTable(masterData);
+//
+//        })
+//        .fail(function( jqxhr, textStatus, error ){
+//            var err = textStatus + ", " + error;
+//            console.log( "Request Failed: " + err );
+//        })
+//        .always(function() {
+//            console.log( "complete" );
+//        });
+//
+//    console.log("final num rows:"+masterData.rows.length);
+
+
+
+}
+
+OZ.sapOrganizer.jsonFormat = function(json){
+    console.log("Format json data");
+
+
+    var data={};
+
+    data.cols=json.cols;
+    data.rows = [];
+
+    var rows=  json.rows;
+    var row;
+    var e;
+
+    for(var i in rows ) {
+        row= rows[i];
+        e={};
+
+        e.id= row.meta.id;
+        e.aris= row.meta.aris;
+        e.track= row.meta.track;
+        e.mode= row.metadata.esr.sender.type+"/"+row.metadata.esr.receiver.type;
+        e.sysSender = row.meta.sender.sysName;
+        e.sysReceiver = row.meta.receiver.sysName;
+
+
+        e.adapSender = row.metadata.ib.adapterSender;
+        e.adapReceiver = row.metadata.ib.adapterReceiver;
+
+        e.senderContact = row.meta.sender.owner;
+        e.receiverContact = row.meta.receiver.owner;
+
+        //e.bsysSender = row.metadata.esr.sender.bs;
+        //e.bsysReceiver = row.metadata.esr.receiver.bs;
+
+
+
+//        console.info(e.id);
+
+        data.rows.push(e);
+
+    }
+
+    return data;
+
+}
+
+OZ.sapOrganizer.buildTable= function(data){
+    console.log("=============");
+
+    var el= $("#dynTable");
+
+    //console.log("build table"+data.cols.id.index);
+
+    //An example with all options.
+    var waTable = el.WATable({
+        pageSize: 8,                //Sets the initial pagesize
+        filter: true,               //Show filter fields
+        columnPicker: true,         //Show the columnPicker button
+        pageSizes: [1,5,8,12,200],  //Set custom pageSizes. Leave empty array to hide button.
+        hidePagerOnEmpty: true,     //Removes the pager if data is empty.
+        checkboxes: true,           //Make rows checkable. (Note. You need a column with the 'unique' property)
+        preFill: true,              //Initially fills the table with empty rows (as many as the pagesize).
+        //url: 'js/data.json',    //Url to a webservice if not setting data manually as we do in this example
+        //urlData: { report:1 }     //Any data you need to pass to the webservice
+        //urlPost: true             //Use POST httpmethod to webservice. Default is GET.
+        types: {                    //Following are some specific properties related to the data types
+            string: {
+                //filterTooltip: "Giggedi..."    //What to say in tooltip when hoovering filter fields. Set false to remove.
+                //placeHolder: "Type here..."    //What to say in placeholder filter fields. Set false for empty.
+            },
+            number: {
+                decimals: 1   //Sets decimal precision for float types
+            },
+            bool: {
+                //filterTooltip: false
+            },
+            date: {
+                utc: true,            //Show time as universal time, ie without timezones.
+                //format: 'yy/dd/MM',   //The format. See all possible formats here http://arshaw.com/xdate/#Formatting.
+                datePicker: true      //Requires "Datepicker for Bootstrap" plugin (http://www.eyecon.ro/bootstrap-datepicker).
+            }
+        },
+        actions: {                //This generates a button where you can add elements.
+            filter: true,         //If true, the filter fields can be toggled visible and hidden.
+            columnPicker: true,   //if true, the columnPicker can be toggled visible and hidden.
+            custom: [             //Add any other elements here. Here is a refresh and export example.
+                $('<a href="#" class="refresh"><i class="icon-refresh"></i>&nbsp;Refresh</a>'),
+                $('<a href="#" class="export_all"><i class="icon-share"></i>&nbsp;Export all rows</a>'),
+                $('<a href="#" class="export_checked"><i class="icon-share"></i>&nbsp;Export checked rows</a>'),
+                $('<a href="#" class="export_filtered"><i class="icon-share"></i>&nbsp;Export filtered rows</a>')
+            ]
+        },
+        tableCreated: function(data) {    //Fires when the table is created / recreated. Use it if you want to manipulate the table in any way.
+            //console.log('table created'); //data.table holds the html table element.
+            //console.log(data);            //'this' keyword also holds the html table element.
+        },
+        rowClicked: function(data) {      //Fires when a row is clicked (Note. You need a column with the 'unique' property).
+            console.log('row clicked');   //data.event holds the original jQuery event.
+            //console.log(data);            //data.row holds the underlying row you supplied.
+            //data.column holds the underlying column you supplied.
+            //data.checked is true if row is checked.
+            //'this' keyword holds the clicked element.
+            if ( $(this).hasClass('userId') ) {
+                data.event.preventDefault();
+                alert('You clicked userId: ' + data.row.userId);
+            }
+        },
+        columnClicked: function(data) {    //Fires when a column is clicked
+            console.log('column clicked');  //data.event holds the original jQuery event
+            console.log(data);              //data.column holds the underlying column you supplied
+            //data.descending is true when sorted descending (duh)
+        },
+        pageChanged: function(data) {      //Fires when manually changing page
+            console.log('page changed');    //data.event holds the original jQuery event
+            console.log(data);              //data.page holds the new page index
+        },
+        pageSizeChanged: function(data) {  //Fires when manually changing pagesize
+            console.log('pagesize changed');//data.event holds teh original event
+            console.log(data);              //data.pageSize holds the new pagesize
+        }
+    }).data('WATable');  //This step reaches into the html data property to get the actual WATable object. Important if you want a reference to it as we want here.
+
+    //Generate some data
+    //var data = this.getData();
+    waTable.setData(data);  //Sets the data.
+    //waTable.setData(data, true); //Sets the data but prevents any previously set columns from being overwritten
+    //waTable.setData(data, false, false); //Sets the data and prevents any previously checked rows from being reset
+
+    var allRows = waTable.getData(false); //Gets the data you previously set.
+    var checkedRows = waTable.getData(true); //Gets the data you previously set, but with checked rows only.
+    var filteredRows = waTable.getData(false, true); //Gets the data you previously set, but with filtered rows only.
+
+    var pageSize = waTable.option("pageSize"); //Get option
+    //waTable.option("pageSize", pageSize); //Set option
+
+    //Example event handler triggered by the custom refresh link above.
+    $('body').on('click', '.refresh', function(e) {
+        e.preventDefault();
+        var data = getData();
+        waTable.setData(data, true);
+    });
+    //Example event handler triggered by the custom export links above.
+    $('body').on('click', '.export_checked, .export_filtered, .export_all', function(e) {
+        e.preventDefault();
+        var elem = $(e.target);
+        var data;
+        if (elem.hasClass('export_all')) data = waTable.getData(false);
+        else if (elem.hasClass('export_checked')) data = waTable.getData(true);
+        else if (elem.hasClass('export_filtered')) data = waTable.getData(false, true);
+        console.log(data.rows.length + ' rows returned');
+        console.log(data);
+        alert(data.rows.length + ' rows returned.\nSee console for details.');
+    });
+
+
+
+
+}
+
+OZ.sapOrganizer.getSampleData1 = function() {
+
+    var data = {
+        cols: {
+            id: {
+                index: 1,
+                friendly: "ID / RICEF",
+                type: "String"
+            },
+            track: {
+                index: 2,
+                friendly: "Track",
+                type: "string"
+            },
+            interfaceType: {
+                index: 3,
+                friendly: "Sync / Async",
+                type: "string"
+            }
+        },
+        rows: [
+            {
+                userId: 1,
+                interfaceType: "Async",
+                track: "Mercaderias"
+            },
+            {
+                userId: 2,
+                interfaceType: "Sync",
+                track: {a:'aw', b:'aaaa'}
+                //track: "Mercaderias"
+            }
+        ]
+    }
+
+
+    return data;
+
+}
+
+
+
+
+OZ.sapOrganizer.getSampleData = function() {
 
     //First define the columns
     var cols = {
@@ -100,81 +376,6 @@ function getData() {
 
     };
 
-
-    var filesrc="js/data.json";
-
-    console.info('GET JSON DATA');
-
-    var jqxhr = $.getJSON( filesrc, function(data) {
-        console.log( "success" );
-    })
-        .done(function() {
-            console.log( "second success" );
-        })
-        .fail(function() {
-            console.log( "error" );
-        })
-        .always(function() {
-            console.log( "complete" );
-        });
-
-//    $.ajax({
-//        dataType: "json",
-//        url: filesrc,
-//        data: data,
-//
-//    });
-//    alert(data.len);
-//
-//    $.getJSON(filesrc, function(data){
-//        // I have placed alert here previously and realized it doesn't go into here
-//        console.info(data.length);
-//        $.each(data.streetCity, function(i,s){
-//            console.info(s);
-//        });
-//    });
-
-//    $.getJSON('~/Content/NBCCJr.json', function (data) {
-//        $.each(data, function(i, dataPoint) {
-//            // Bla
-//        });
-//    });
-
-
-    var data = {
-        cols: {
-            id: {
-                index: 1,
-                friendly: "ID / RICEF",
-                type: "String"
-            },
-            track: {
-                index: 2,
-                friendly: "Track",
-                type: "string"
-            },
-            interfaceType: {
-                index: 3,
-                friendly: "Sync / Async",
-                type: "string"
-            }
-        },
-        rows: [
-            {
-                userId: 1,
-                interfaceType: "Async",
-                track: "Mercaderias"
-            },
-            {
-                userId: 2,
-                interfaceType: "Sync",
-                track: {a:'aw', b:'aaaa'}
-                //track: "Mercaderias"
-            }
-        ]
-    }
-
-
     return data;
 }
 
@@ -205,3 +406,83 @@ function elfName() {
     }
     return name;
 }
+
+
+/**
+ * Orbital Tabs style script
+ * @type {{}}
+ */
+
+OZ.view = OZ.view = {};
+
+OZ.view.loadTabs = function(){
+
+    console.log('========= TBAS');
+
+    // get tab container
+    var container = document.getElementById("tabContainer");
+    // set current tab
+    var navitem = container.querySelector(".tabs ul li");
+    //store which tab we are on
+    var ident = navitem.id.split("_")[1];
+    navitem.parentNode.setAttribute("data-current",ident);
+    //set current tab with class of activetabheader
+    navitem.setAttribute("class","tabActiveHeader");
+
+    //hide two tab contents we don't need
+    var pages = container.querySelectorAll(".tabpage");
+    for (var i = 1; i < pages.length; i++) {
+        pages[i].style.display="none";
+    }
+
+    //this adds click event to tabs
+    var tabs = container.querySelectorAll(".tabs ul li");
+    for (var i = 0; i < tabs.length; i++) {
+        tabs[i].onclick=displayPage;
+    }
+
+    function displayPage() {
+        var current = this.parentNode.getAttribute("data-current");
+        //remove class of activetabheader and hide old contents
+        document.getElementById("tabHeader_" + current).removeAttribute("class");
+        document.getElementById("tabpage_" + current).style.display="none";
+
+        var ident = this.id.split("_")[1];
+        //add class of activetabheader to new active tab and show contents
+        this.setAttribute("class","tabActiveHeader");
+        document.getElementById("tabpage_" + ident).style.display="block";
+        this.parentNode.setAttribute("data-current",ident);
+    }
+}
+
+jQuery.fn.fadeThenSlideToggle = function(speed, easing, callback) {
+    if (this.is(":hidden")) {
+        return this.slideDown(speed, easing).fadeTo(speed, 1, easing, callback);
+    } else {
+        return this.fadeTo(speed, 0, easing).slideUp(speed, easing, callback);
+    }
+};
+
+jQuery.fn.slideAndFadeToggle = function(speed, easing, callback) {
+    if (this.is(":hidden")) {
+
+        this.animate({ opacity: 1, top: "0px", height: 'toggle'}, 400, function() {/* Animation complete.*/});
+
+    } else {
+        this.animate({ opacity: 0, top: "0px",height: 'toggle'}, 400, function() {/* Animation complete.*/});
+    }
+};
+
+jQuery.fn.slideAndFadeToggleB = function(speed, easing, callback) {
+    //  console.info("isHidenn: "+this.is(":hidden"));
+    if (this.is(":hidden")) {
+
+        //this.animate({ left: "0px"});
+        this.css('display', '');
+        this.animate({ opacity: 1, left: "0px"}, 400,function() {$(this).css('display', '');});
+
+    } else {
+
+        this.animate({ opacity: 0, left: "600px"}, 400,function() {$(this).css('display', 'none');});
+    }
+};
